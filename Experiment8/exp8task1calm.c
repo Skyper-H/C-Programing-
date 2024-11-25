3e0f3cab703bdcf81a44afe1394807cefe07918e
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 22:51:50 +0530

    Update exp8task1calm.c

--- Experiment8/exp8task1calm.c
+++ Experiment8/exp8task1calm.c
@@ -1,13 +1,8 @@
-/*Aim : WAP to find the length of a string without using library function.
-Name : Mohammed Aquib 
-Roll no : 06
-UIN : 241A006
-Division : A*/
 #include<stdio.h>
 int main()
 {
     char str[100];
-    int length=0;   //variable
+    int length=0;   
     printf("\t\t*** String Length Finder ***\n\n\n");
     printf("Enter Your String : ");
     gets(str);
