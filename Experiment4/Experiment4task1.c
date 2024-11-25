f2fbac59bf5318f43e7a8de4dd337543ad4c62e9
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 22:37:28 +0530

    Update Experiment4task1.c

--- Experiment4/Experiment4task1.c
+++ Experiment4/Experiment4task1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 void is_prime(int num);
-int main (){
+int main ()
+{
     printf("\t\t\t *** Prime Number Finder *** \n\n");1
 int s_n,e_n,i,c=0;
 i= s_n<=1?2:s_n;
@@ -11,16 +12,24 @@ scanf("%d",&e_n);
 printf("The Prime number Between %d and %d is :",s_n,e_n);
 i= s_n<=1?2:s_n;
 while (i <=e_n){
-if(is_prime_number(i)){
+if(is_prime_number(i))
+{
     printf("%d,",i);
     c++;
 }
  i++;
 }
 printf("   \n%d",c);
-return 0;}
-int is_prime_number(int num){
-    for (int i=2; i<num; i++) {
-      if (num % i== 0){
-        return 0;}}
-     return 1;}
+return 0;
+}
+int is_prime_number(int num)
+{
+    for (int i=2; i<num; i++) 
+    {
+      if (num % i== 0)
+      {
+        return 0;
+      }
+    }
+     return 1;
+}
