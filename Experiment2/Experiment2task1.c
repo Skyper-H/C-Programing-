70e686cdad8e9c148645c23a55637fa88a91d0ae
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 22:16:11 +0530

    Update Experiment2task1.c

--- Experiment2/Experiment2task1.c
+++ Experiment2/Experiment2task1.c
@@ -1,14 +1,19 @@
-/*WAP to find if entered number is
-even or odd.*/
 #include<stdio.h>
-int main(){
+int main()
+{
 printf("\t\t\t ***Even odd Finder*** \n\n\n");
 
-int number,remaider ;
+int number,remaider;
 printf("Enter the desired number: ");
 scanf("%d",&number);
 remaider = number%2;
-if(remaider == 0){
+if(remaider == 0)
+{
     printf("The entered number %d is even",number);
 }
-else{printf("The entered number %d is odd",number);}}
+else
+{
+    printf("The entered number %d is odd",number);
+}
+return 0;
+}
