67652e3b42f77b834306738785f1776a87f52431
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 23:00:24 +0530

    Update ezp10tsk2.c

--- Experiment10/ezp10tsk2.c
+++ Experiment10/ezp10tsk2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 
     int n;
 
@@ -17,15 +18,17 @@ int main() {
 
     printf("Enter the elements of the array:\n");
 
-    for (int i = 0; i < n; i++) {
-
+    for (int i = 0; i < n; i++) 
+    {
+        printf("id%d", i + 1);
         scanf("%d", ptr + i);
     }
 
 
     printf("\nArray elements in reverse order:\n");
 
-    for (int i = n - 1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--) 
+    {
         printf("%d ", *(ptr + i));
     }
 
