ea2c2f8d4179b3d7681ed93cef45a2bf5b947af0
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 14:03:19 +0530

    Add files via upload

---
+++ Experiment1/Experiment1task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*Aim: Write a program to print your name, age, class, division and UIN on the screen.
+Use escape sequences to properly format the output.*/
+
+int main() {
+
+    printf("Student Name\t: Mohd Emaad Akhter\n");
+    printf("Student UIN ID\t: 241A019\n");
+    printf("Student Class\t: FY AIDS\n");
+    printf("Student Division: A1\n");
+    printf("Student Age\t: 18\n");
+
+
+    return 0;
+}
---
+++ Experiment1/Experiment1task2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*Aim: Write a program to get students PCM marks from the user and find the average. Use conditional
+operator to print if the student is eligible for admission. Eligibility criteria is 50% in
+PCM.*/
+
+int main() {
+  double P,C,M,PCM;
+  printf("Please enter HSC marks\n");
+  printf("Enter Physics marks\t   :");
+  scanf("%lf", &P);
+  printf("Enter Chemistry marks      :");
+  scanf("%lf", &C);
+  printf("Enter Maths marks\t   :");
+  scanf("%lf", &M);
+  PCM=(P+C+M)/3;
+  printf("The Avg of your PCM Marks:%f",PCM);
+  (PCM>=50.0)?printf("You are Eligible for college admission\n"):
+  printf("you are not eligible for college admission");
+    return 0;
+}
