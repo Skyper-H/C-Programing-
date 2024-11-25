642e0da98bc6f787a784997c9d512a53dfb29084
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-26 00:32:18 +0530

    Update exp11 task 1.c

--- Experiment11/exp11 task 1.c
+++ Experiment11/exp11 task 1.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-
-// Define a structure to hold employee details
-struct employee {
+struct employee 
+{
     int id;
     char name[50];
     float salary;
 };
 
-void add_employee() {
+void add_employee() 
+{
     struct employee emp;
     FILE *file = fopen("employee_database.txt", "a");
 
-    if (file == NULL) {
+    if (file == NULL) 
+    {
         printf("Error opening file!\n");
         return;
     }
-
-    // Input employee details
     printf("Enter Employee ID: ");
     scanf("%d", &emp.id);
     printf("Enter Employee Name: ");
@@ -24,18 +23,18 @@ void add_employee() {
     gets(emp.name);
     printf("Enter Employee Salary: ");
     scanf("%f", &emp.salary);
-
-    // Write employee data to the file
     fprintf(file, "%d %.2f %s\n", emp.id, emp.salary, emp.name);
     fclose(file);
     printf("Employee record added successfully!\n");
 }
-void display_employees() {
+void display_employees() 
+{
     struct employee emp;
 
     FILE *file = fopen("employee_database.txt", "r");
 
-    if (file == NULL) {
+    if (file == NULL) 
+    {
         printf("No employee records found!\n");
         return;
     }
@@ -43,21 +42,19 @@ void display_employees() {
     printf("\nEmployee Records:\n");
     printf("ID\tName\t\tSalary\n");
     printf("------------------------------------\n");
-
-    // Read and display each employee record from the file
-    while (fscanf(file, "%d %f %[^\n]", &emp.id, &emp.salary, emp.name) != EOF) {
+    while (fscanf(file, "%d %f %[^\n]", &emp.id, &emp.salary, emp.name) != EOF) 
+    {
         printf("%d\t%-15s\t%.2f\n", emp.id, emp.name, emp.salary);
     }
 
     fclose(file);
 }
-int main(){
+int main()
+{
     int choice;
-
     printf("\t\t * Employee Database * \n\n");
-
-    while (1) {
-        // Menu for user
+    while (1) 
+    {
         printf("\nEmployee Database Menu:\n");
         printf("1. Add Employee Record\n");
         printf("2. Display Employee Records\n");
@@ -65,7 +62,8 @@ int main(){
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
+        switch (choice) 
+        {
             case 1:
                 add_employee();
                 break;
