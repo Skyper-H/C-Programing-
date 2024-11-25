adf3926c4b8dc13690ebac9a3e0d701053d5e139
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 22:17:56 +0530

    Update Experiment2task2.c

--- Experiment2/Experiment2task2.c
+++ Experiment2/Experiment2task2.c
@@ -1,17 +1,12 @@
-/*WAP to find the sum of all the odd
-numbers between numbers entered by the
-User.*/
 #include<stdio.h>
-int main(){
+int main()
+{
 printf("\t\t\t ***Sum of odd numbers*** \n\n\n");
- // initialization of all the variables
 int low_range,high_range,remainder,sum=0,odd_num;
- // reading all  the inputs
 printf("Enter the low end of the range\t:");
  scanf("%d",&low_range);
 printf("Enter the high end of the range\t:");
  scanf("%d",&high_range);
- // main logic
 if (low_range<high_range)
 {
  for ( odd_num=low_range ;odd_num <= high_range; odd_num++)
@@ -23,7 +18,6 @@ if (low_range<high_range)
            sum =odd_num+sum ;
           }
       }
-      //result
      printf("The sum of all odd numbers in the given range of %d to %d is equal to: %d\n",low_range,high_range, sum );
 }
 else
