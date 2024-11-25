12214de95b8033eb3e6142e5200610cb1ade9cfa
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 14:06:33 +0530

    Update Experiment1task1.c

--- Experiment1/Experiment1task1.c
+++ Experiment1/Experiment1task1.c
@@ -5,10 +5,10 @@ Use escape sequences to properly format the output.*/
 
 int main() {
 
-    printf("Student Name\t: Mohd Emaad Akhter\n");
-    printf("Student UIN ID\t: 241A019\n");
+    printf("Student Name\t: ABDUL GHANI IFTIKHAR AHMAD \n");
+    printf("Student UIN ID\t: 241A056\n");
     printf("Student Class\t: FY AIDS\n");
-    printf("Student Division: A1\n");
+    printf("Student Division: A3\n");
     printf("Student Age\t: 18\n");
 
 
