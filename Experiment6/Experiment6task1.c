0f925925fd60bf5fc19436fc53c57a02d15e3b24
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 22:43:00 +0530

    Add files via upload

---
+++ Experiment6/Experiment6task1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void counter(){
+static int count =0;
+count++;
+printf("Counter function is called:%d\n",count);
+}
+
+int main(){
+int n,i=1;
+printf("\t\t\t***Storage Counter***\n\n");
+printf("Enter The Number Of Calls:");
+scanf("%d",&n);
+while (i<=n){
+    counter();
+    i++;
+}
+return 0;
+}
+
