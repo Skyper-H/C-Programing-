c33e150c620ed9a29dc18f29b9bbfc7539e153fe
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-25 22:45:35 +0530

    Update Experiment7task1.c

--- Experiment7/Experiment7task1.c
+++ Experiment7/Experiment7task1.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
-int main(){
+int main()
+{
 int n,i,a[100],large;
   printf("Enter the lenght:");
     scanf("%d",&n);
-for(i=0;i<=n-1;i++){
+for(i=0;i<=n-1;i++)
+{
     printf("Enter the value:");
     scanf("%d",&a[i]);
 }
 large=a[0];
-for(i=0;i<=n-1;i++){
- if(a[i]>large){
+for(i=0;i<=n-1;i++)
+{
+ if(a[i]>large)
+ {
     large=a[i];
  }
 }
