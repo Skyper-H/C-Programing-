58f26fd52d06ee649648bdcfca9149c12d3ed1c4
Abdul.G <abdulghani@eng.rizvi.edu.in>
2024-11-29 02:17:44 +0530

    Update Experiment1task1.c

--- Experiment1/Experiment1task1.c
+++ Experiment1/Experiment1task1.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-
-/*Aim: Write a program to print your name, age, class, division and UIN on the screen.
-Use escape sequences to properly format the output.*/
-
-int main() {
+int main() 
+{
 
     printf("Student Name\t: ABDUL GHANI IFTIKHAR AHMAD \n");
     printf("Student UIN ID\t: 241A056\n");
